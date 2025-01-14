19d2211835288e4d99e32a682105d597b097ab73
kimyu0218 <serena35@naver.com>
2025-01-15 01:51:50 +0900

    [#3] chore: api 문서화 관련 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.4.1"
     id("io.spring.dependency-management") version "1.1.7"
+    id("com.epages.restdocs-api-spec") version "0.19.4"
 }
 
 group = "org.nexters"
@@ -29,10 +30,39 @@ dependencies {
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.projectlombok:lombok")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // restdocs-api-spec
+    testImplementation("com.epages:restdocs-api-spec-mockmvc:0.19.4")
+
+    // spring-rest-docs
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
+val oasDir = file("build/api-spec")
+val swaggerUiDir = file("swagger-ui")
+
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.register<Copy>("copyOasToSwaggerUi") {
+    dependsOn("openapi3")
+    from("$oasDir/openapi3.yaml")
+    into(swaggerUiDir)
+}
+
+tasks.named("build") {
+    finalizedBy("copyOasToSwaggerUi")
+}
+
+openapi3 {
+    this.setServer("http://localhost:8080")
+    title = "Jaknaeso API"
+    description = "Jaknaeso API description"
+    version = "0.1.0"
+    format = "yaml"
 }
