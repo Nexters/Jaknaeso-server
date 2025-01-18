dc1d5e9844f50b05d1e9403991c3b1b6a94d36bb
NaMinhyeok <nmh9097@gmail.com>
2025-01-18 23:09:53 +0900

    [#7] feat: 스웨거 서버 추가 구성

---
+++ terraform/modules/server/data.tf
@@ -0,0 +1,49 @@
+data "ncloud_server_image" "server_image" {
+  filter {
+    name = "product_name"
+    values = ["ubuntu-20.04"]
+  }
+}
+
+data "ncloud_server_image_numbers" "kvm_image" {
+  server_image_name = "ubuntu-22.04"
+  filter {
+    name = "hypervisor_type"
+    values = ["KVM"]
+  }
+}
+
+data "ncloud_server_specs" "kvm-spec" {
+  filter {
+    name = "server_spec_code"
+    values = ["mi1-g3"]
+  }
+}
+
+data "ncloud_server_product" "product" {
+  server_image_product_code = data.ncloud_server_image.server_image.id
+
+  filter {
+    name  = "product_code"
+    values = ["SSD"]
+    regex = true
+  }
+
+  filter {
+    name = "cpu_count"
+    values = ["2"]
+  }
+  filter {
+    name = "memory_size"
+    values = ["4GB"]
+  }
+  filter {
+    name = "product_type"
+    values = ["HICPU"]
+  }
+}
+
+data "ncloud_root_password" "default" {
+  server_instance_no = ncloud_server.jaknaeso_server.instance_no
+  private_key        = ncloud_login_key.jaknaeso_server_login_key.private_key
+}
---
+++ terraform/modules/server/network.tf
@@ -0,0 +1,59 @@
+resource "ncloud_access_control_group" "jaknaeso_server_acg_01" {
+  vpc_no      = var.vpc_no
+  name        = "${var.name_terra}-acg-01"
+  description = "${var.name_terra} Access Control Group 01"
+}
+
+resource "ncloud_access_control_group_rule" "jaknaeso_server_acg_01_rule_01" {
+  access_control_group_no = ncloud_access_control_group.jaknaeso_server_acg_01.id
+
+  inbound {
+    protocol    = "TCP"
+    ip_block    = "0.0.0.0/0"
+    port_range  = "22"
+    description = "SSH access"
+  }
+
+  inbound {
+    protocol    = "TCP"
+    ip_block    = "0.0.0.0/0"
+    port_range  = "8080"
+    description = "Backend server port"
+  }
+
+  outbound {
+    protocol    = "TCP"
+    ip_block    = "0.0.0.0/0"
+    port_range  = "1-65535"
+    description = "All TCP outbound"
+  }
+
+  outbound {
+    protocol    = "UDP"
+    ip_block    = "0.0.0.0/0"
+    port_range  = "1-65535"
+    description = "All UDP outbound"
+  }
+}
+
+resource "ncloud_network_interface" "jaknaeso_server_nic_01" {
+  access_control_groups = [ncloud_access_control_group.jaknaeso_server_acg_01.id]
+  subnet_no = var.subnet_public_id
+  name      = "${var.name_terra}-jaknaeso-nic-01"
+}
+
+resource "ncloud_network_interface" "jaknaeso_swagger_nic_01" {
+  access_control_groups = [ncloud_access_control_group.jaknaeso_server_acg_01.id]
+  subnet_no = var.subnet_public_id
+  name      = "${var.name_terra}-swagger-nic-01"
+}
+
+resource "ncloud_public_ip" "jaknaeso_public_ip" {
+  server_instance_no = ncloud_server.jaknaeso_server.instance_no
+  description        = "for ${ncloud_server.jaknaeso_server.name} public ip"
+}
+
+resource "ncloud_public_ip" "jaknaeso_swagger_public_ip" {
+  server_instance_no = ncloud_server.jaknaeso_swagger_server.instance_no
+  description        = "for ${ncloud_server.jaknaeso_swagger_server.name} public ip"
+}
