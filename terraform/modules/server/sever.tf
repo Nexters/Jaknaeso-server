a0a943d6911034ab16d6aa2149700732594c56ee
NaMinhyeok <nmh9097@gmail.com>
2025-01-21 01:02:17 +0900

    [#7] feat: swagger 서버 등록

--- terraform/modules/server/sever.tf
+++ terraform/modules/server/sever.tf
@@ -10,3 +10,17 @@ resource "ncloud_server" "jaknaeso_server" {
     order                = 0
   }
 }
+
+resource "ncloud_server" "jaknaeso_swagger_server" {
+  subnet_no           = var.subnet_public_id
+  name                = "jaknaeso-swagger-server"
+  login_key_name      = ncloud_login_key.jaknaeso_server_login_key.key_name
+  server_spec_code    = data.ncloud_server_specs.kvm-spec.server_spec_list.0.server_spec_code
+  server_image_number = data.ncloud_server_image_numbers.kvm_image.image_number_list.0.server_image_number
+  fee_system_type_code = "FXSUM"
+
+  network_interface {
+    network_interface_no = ncloud_network_interface.jaknaeso_swagger_nic_01.id
+    order                = 0
+  }
+}
