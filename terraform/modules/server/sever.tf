e04c8876256aaf9a8fd2ae454ecabc762bb7d415
NaMinhyeok <nmh9097@gmail.com>
2025-01-20 13:08:11 +0900

    [#7] fix: swagger server 테라폼 제외

--- terraform/modules/server/sever.tf
+++ terraform/modules/server/sever.tf
@@ -10,16 +10,3 @@ resource "ncloud_server" "jaknaeso_server" {
     order                = 0
   }
 }
-
-resource "ncloud_server" "jaknaeso_swagger_server" {
-  subnet_no           = var.subnet_public_id
-  name                = "jaknaeso-swagger-server"
-  login_key_name      = ncloud_login_key.jaknaeso_server_login_key.key_name
-  server_spec_code    = data.ncloud_server_specs.kvm-spec.server_spec_list.0.server_spec_code
-  server_image_number = data.ncloud_server_image_numbers.kvm_image.image_number_list.0.server_image_number
-
-  network_interface {
-    network_interface_no = ncloud_network_interface.jaknaeso_swagger_nic_01.id
-    order                = 0
-  }
-}
