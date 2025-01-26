5c8ebeffd2759b44ffa671f4aa42615c0288916e
kimyu0218 <serena35@naver.com>
2025-01-27 01:42:16 +0900

    [#15] feat: MediaTypeBuilder 클래스 작성

---
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/common/http/MediaTypeValueBuilder.java
@@ -0,0 +1,48 @@
+package org.nexters.jaknaesocore.common.http;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class MediaTypeValueBuilder {
+
+  private String mediaType;
+  private String charset;
+
+  private MediaTypeValueBuilder(final String mediaType) {
+    this.mediaType = mediaType;
+  }
+
+  public static MediaTypeValueBuilder builder() {
+    return new MediaTypeValueBuilder();
+  }
+
+  public static MediaTypeValueBuilder builder(final String mediaType) {
+    return new MediaTypeValueBuilder(mediaType);
+  }
+
+  public MediaTypeValueBuilder mediaType(final String mediaType) {
+    this.mediaType = mediaType;
+    return this;
+  }
+
+  public MediaTypeValueBuilder charset(final String charset) {
+    this.charset = charset;
+    return this;
+  }
+
+  public String build() {
+    if (!hasField(mediaType)) {
+      throw new IllegalStateException("mediaType 값이 설정되지 않았습니다.");
+    }
+
+    StringBuilder s = new StringBuilder(mediaType);
+    if (hasField(charset)) {
+      s.append(";charset=").append(charset);
+    }
+    return s.toString();
+  }
+
+  private boolean hasField(final String field) {
+    return field != null && !field.isBlank();
+  }
+}
---
+++ jaknaeso-server/src/test/java/org/nexters/jaknaesoserver/common/http/MediaTypeValueBuilderTest.java
@@ -0,0 +1,29 @@
+package org.nexters.jaknaesoserver.common.http;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.nexters.jaknaesocore.common.http.MediaTypeValueBuilder;
+
+public class MediaTypeValueBuilderTest {
+
+  @DisplayName("MediaType 값을 charset과 함께 직렬화할 수 있다")
+  @Test
+  void serializeMediaTypeWithCharset() {
+    String actual1 = MediaTypeValueBuilder.builder(APPLICATION_JSON_VALUE)
+        .charset("utf-8")
+        .build();
+    String actual2 = MediaTypeValueBuilder.builder(APPLICATION_FORM_URLENCODED_VALUE)
+        .charset("utf-8")
+        .build();
+
+    assertAll(
+        () -> assertThat(actual1).isEqualTo("application/json;charset=utf-8"),
+        () -> assertThat(actual2).isEqualTo("application/x-www-form-urlencoded;charset=utf-8")
+    );
+  }
+}
