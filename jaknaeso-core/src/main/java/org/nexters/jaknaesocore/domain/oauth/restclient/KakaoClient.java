5096c41daaa9af79b2c7b3a1ebccbd7fca451e9a
kimyu0218 <serena35@naver.com>
2025-01-28 03:34:37 +0900

    [#15] feat: 카카오 로그인 API를 호출하는 KakaoClient 정의

---
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/oauth/restclient/KakaoClient.java
@@ -0,0 +1,19 @@
+package org.nexters.jaknaesocore.domain.oauth.restclient;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+
+import org.nexters.jaknaesocore.domain.oauth.restclient.dto.KakaoUserInfoResponse;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+
+@HttpExchange(url = "https://kapi.kakao.com")
+public interface KakaoClient {
+
+  @GetExchange("/v2/user/me")
+  KakaoUserInfoResponse requestUserInfo(
+      @RequestHeader(AUTHORIZATION) String authorization,
+      @RequestHeader(CONTENT_TYPE) String contentType
+  );
+}
---
+++ jaknaeso-server/src/test/java/org/nexters/jaknaesoserver/domain/oauth/restclient/KakaoClientTest.java
@@ -0,0 +1,30 @@
+package org.nexters.jaknaesoserver.domain.oauth.restclient;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.nexters.jaknaesocore.common.http.MediaTypeValueBuilder;
+import org.nexters.jaknaesocore.domain.oauth.restclient.KakaoClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestClientException;
+
+@SpringBootTest
+class KakaoClientTest {
+
+  @Autowired
+  KakaoClient kakaoClient;
+
+  @DisplayName("Authorization 헤더 없이 카카오 API를 호출하면 RestClientException을 반환한다.")
+  @Test
+  void failToRequestUserInfoWithoutAuthorization() {
+    String mediaType = MediaTypeValueBuilder.builder(APPLICATION_FORM_URLENCODED_VALUE)
+        .charset("utf-8")
+        .build();
+
+    assertThatThrownBy(() -> kakaoClient.requestUserInfo("", mediaType))
+        .isInstanceOf(RestClientException.class);
+  }
+}
