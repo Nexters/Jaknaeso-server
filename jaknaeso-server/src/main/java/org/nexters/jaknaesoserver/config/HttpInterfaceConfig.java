940c2e02150cffc992a49f1ffc6d9fe26ca3bc91
kimyu0218 <serena35@naver.com>
2025-01-27 17:46:49 +0900

    [#15] feat: basePackage 지정

--- jaknaeso-server/src/main/java/org/nexters/jaknaesoserver/config/HttpInterfaceConfig.java
+++ jaknaeso-server/src/main/java/org/nexters/jaknaesoserver/config/HttpInterfaceConfig.java
@@ -1,11 +1,10 @@
 package org.nexters.jaknaesoserver.config;
 
 import java.util.Set;
-import org.nexters.jaknaesoserver.common.httpinterface.HttpInterfaceFactory;
-import org.nexters.jaknaesoserver.common.httpinterface.HttpInterfaceFactoryBeanPostProcessor;
-import org.nexters.jaknaesoserver.common.httpinterface.HttpInterfaceFinder;
+import org.nexters.jaknaesocore.common.httpinterface.HttpInterfaceFactory;
+import org.nexters.jaknaesocore.common.httpinterface.HttpInterfaceFactoryBeanPostProcessor;
+import org.nexters.jaknaesocore.common.httpinterface.HttpInterfaceFinder;
 import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,19 +16,9 @@ public class HttpInterfaceConfig {
   public HttpInterfaceFactoryBeanPostProcessor httpInterfaceFactoryBeanPostProcessor
       (final ApplicationContext context, final HttpInterfaceFactory httpInterfaceFactory) {
     Set<BeanDefinition> beanDefinitions = findHttpInterfaceBeanDefinitions(
-        findBasePackage(context), context
+        "org.nexters.jaknaesocore", context
     );
     return new HttpInterfaceFactoryBeanPostProcessor(httpInterfaceFactory, beanDefinitions);
-  }
-
-  private String findBasePackage(final ApplicationContext context) {
-    return context.getBeansWithAnnotation(SpringBootApplication.class)
-        .values()
-        .stream()
-        .map(it -> it.getClass().getPackage().getName())
-        .findFirst()
-        .orElseThrow(
-            () -> new IllegalStateException("SpringBootApplication 애노테이션이 붙은 클래스가 존재하지 않습니다."));
   }
 
   Set<BeanDefinition> findHttpInterfaceBeanDefinitions(
