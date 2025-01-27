816a791b1297863fe14ff8a08792a972d4deb350
kimyu0218 <serena35@naver.com>
2025-01-28 03:31:51 +0900

    [#15] refactor: RestClientErrorHandler 매개변수 타입 수정

--- jaknaeso-core/src/main/java/org/nexters/jaknaesocore/common/httpinterface/errorhandler/RestClientErrorHandler.java
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/common/httpinterface/errorhandler/RestClientErrorHandler.java
@@ -1,8 +1,10 @@
 package org.nexters.jaknaesocore.common.httpinterface.errorhandler;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpResponse;
 
 public interface RestClientErrorHandler {
 
-  void handleError(HttpStatusCode statusCode, Object request, Object response);
+  void handleError(HttpStatusCode statusCode, HttpRequest request, ClientHttpResponse response);
 }
