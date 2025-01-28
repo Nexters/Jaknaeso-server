118bc4be05871b5ed19526140d5649221558eb95
kimyu0218 <serena35@naver.com>
2025-01-29 02:06:48 +0900

    [#15] chore: ddl 파일 추가

---
+++ jaknaeso-server/src/main/resources/sql/issue#015/001_create_table_member.sql
@@ -0,0 +1,7 @@
+create table member if not exists member
+(
+  id              bigint        auto_increment primary key,
+  created_at      timestamp(6),
+  updated_at      timestamp(6),
+  deleted_at      timestamp(6)
+);
