cc5650643a1606aa762c23346a23414a892d1e6e
kimyu0218 <serena35@naver.com>
2025-01-29 02:04:46 +0900

    [#15] feat: SpringBootConfig 정의
    - org.nexters.jaknaesocore 테스트 실행 시 SpringBootConfiguration 찾지 못함
    - 이를 해결하기 위해 루트 레벨에 SpringBootConfig 설정 파일 작성

---
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/SpringBootConfig.java
@@ -0,0 +1,8 @@
+package org.nexters.jaknaesocore;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = "org.nexters")
+@SpringBootConfiguration
+public class SpringBootConfig {}
