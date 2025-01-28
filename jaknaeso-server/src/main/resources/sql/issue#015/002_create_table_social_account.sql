a71e70a2e5fb646cbbcbb5cc24d531190b03bb26
kimyu0218 <serena35@naver.com>
2025-01-29 02:06:48 +0900

    [#15] chore: ddl 파일 추가

---
+++ jaknaeso-server/src/main/resources/sql/issue#015/002_create_table_social_account.sql
@@ -0,0 +1,12 @@
+create table social_account if not exists social_account
+(
+  id              bigint        auto_increment primary key,
+  oauth_id        varchar(255)  not null,
+  social_provider varchar(10),
+  member_id       bigint,
+  created_at      timestamp(6),
+  updated_at      timestamp(6),
+  deleted_at      timestamp(6),
+  created_by      bigint,
+  updated_by      bigint
+);
