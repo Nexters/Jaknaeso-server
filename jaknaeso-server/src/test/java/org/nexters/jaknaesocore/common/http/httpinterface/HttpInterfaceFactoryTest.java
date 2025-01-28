3361ef4da173bf3c9c806b8d130db858e8aa31f0
kimyu0218 <serena35@naver.com>
2025-01-29 01:57:50 +0900

    [#15] refactor: assertj -> BDD assertions

--- jaknaeso-server/src/test/java/org/nexters/jaknaesocore/common/http/httpinterface/HttpInterfaceFactoryTest.java
+++ jaknaeso-server/src/test/java/org/nexters/jaknaesocore/common/http/httpinterface/HttpInterfaceFactoryTest.java
@@ -1,7 +1,7 @@
 package org.nexters.jaknaesocore.common.http.httpinterface;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.assertj.core.api.BDDAssertions.thenCode;
+import static org.assertj.core.api.BDDAssertions.thenThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,20 +16,20 @@ class HttpInterfaceFactoryTest {
   @DisplayName("HttpInterface를 위한 프록시 객체를 생성한다.")
   @Test
   void createHttpInterface() {
-    assertDoesNotThrow(() -> httpInterfaceFactory.create(KakaoClient.class));
+    thenCode(() -> httpInterfaceFactory.create(KakaoClient.class)).doesNotThrowAnyException();
   }
 
   @DisplayName("HttpExchange 애노테이션이 없는 HttpInterface는 프록시 객체 생성에 실패한다.")
   @Test
   void failToCreateHttpInterfaceWithoutHttpExchange() {
-    assertThatThrownBy(() -> httpInterfaceFactory.create(NoHttpExchangeHttpInterface.class))
+    thenThrownBy(() -> httpInterfaceFactory.create(NoHttpExchangeHttpInterface.class))
         .isInstanceOf(IllegalStateException.class);
   }
 
   @DisplayName("HttpExchange 애노테이션의 url 값이 비어있는 HttpInterface는 프록시 객체 생성에 실패한다.")
   @Test
   void failToCreateHttpInterfaceWithoutUrl() {
-    assertThatThrownBy(() -> httpInterfaceFactory.create(EmptyUrlHttpInterface.class))
+    thenThrownBy(() -> httpInterfaceFactory.create(EmptyUrlHttpInterface.class))
         .isInstanceOf(IllegalArgumentException.class);
   }
 }
