dcd9e56fd9399a663fb465f8144aa1f48137e3dc
kimyu0218 <serena35@naver.com>
2025-01-29 17:42:27 +0900

    [#15] feat: 토큰 서비스 병합

--- jaknaeso-server/src/main/java/org/nexters/jaknaesoserver/domain/auth/service/JwtService.java
+++ jaknaeso-server/src/main/java/org/nexters/jaknaesoserver/domain/auth/service/JwtService.java
@@ -13,8 +13,15 @@ public class JwtService {
   private final JwtParser jwtParser;
   private final JwtProvider jwtProvider;
 
+  public TokenResponse issueToken(final Long userId) {
+    String accessToken = jwtProvider.generateAccessToken(userId);
+    String refreshToken = jwtProvider.generateRefreshToken(userId);
+    return TokenResponse.of(userId, accessToken, refreshToken);
+  }
+
   public TokenResponse reissueToken(final String bearerRefreshToken) {
     String refreshToken = bearerRefreshToken.replace(BEARER_PREFIX, "");
+
     Long userId = jwtParser.extractIdFromToken(refreshToken);
     String accessToken = jwtProvider.generateAccessToken(userId);
     String newRefreshToken = jwtProvider.generateRefreshToken(userId);
