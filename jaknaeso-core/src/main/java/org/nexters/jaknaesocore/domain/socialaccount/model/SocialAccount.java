cca99820edef2981febe0a7938f1e675e3b06816
kimyu0218 <serena35@naver.com>
2025-01-30 15:11:43 +0900

    [#15] refactor: eager loading -> lazy loading + fetch join
    social account의 member가 null일 수 있으므로 left join fetch 사용

--- jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/socialaccount/model/SocialAccount.java
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/socialaccount/model/SocialAccount.java
@@ -3,6 +3,7 @@ package org.nexters.jaknaesocore.domain.socialaccount.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -23,7 +24,7 @@ public class SocialAccount extends BaseAuditableEntity {
   @Enumerated(EnumType.STRING)
   private SocialProvider socialProvider;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "member_id")
   private Member member;
 
--- jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/socialaccount/repository/SocialAccountRepository.java
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/socialaccount/repository/SocialAccountRepository.java
@@ -4,9 +4,13 @@ import java.util.Optional;
 import org.nexters.jaknaesocore.domain.socialaccount.model.SocialAccount;
 import org.nexters.jaknaesocore.domain.socialaccount.model.SocialProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SocialAccountRepository extends JpaRepository<SocialAccount, Long> {
 
+  @Query(
+      "SELECT sa FROM SocialAccount sa LEFT JOIN FETCH sa.member "
+          + "WHERE sa.oauthId = :oauthId AND sa.socialProvider = :socialProvider")
   Optional<SocialAccount> findByOauthIdAndSocialProvider(
       final String oauthId, final SocialProvider socialProvider);
 
