94959c118bc7007805f421a023af900610d6f016
NaMinhyeok <nmh9097@gmail.com>
2025-01-31 01:58:26 +0900

    [#29] test: contextload 비활성화

--- jaknaeso-server/src/test/java/org/nexters/jaknaesoserver/JaknaesoserverApplicationTest.java
+++ jaknaeso-server/src/test/java/org/nexters/jaknaesoserver/JaknaesoserverApplicationTest.java
@@ -2,9 +2,11 @@ package org.nexters.jaknaesoserver;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class JaknaesoserverApplicationTest {
 
