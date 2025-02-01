2cb79e0920f0511cc12032c7b8d614871a7f447d
NaMinhyeok <nmh9097@gmail.com>
2025-02-01 17:41:46 +0900

    [#22] feat: authenticationprincipal 사용 할 수 있도록 추가

---
+++ jaknaeso-server/src/main/java/org/nexters/jaknaesoserver/domain/auth/model/CustomUserDetails.java
@@ -0,0 +1,31 @@
+package org.nexters.jaknaesoserver.domain.auth.model;
+
+import java.util.Collection;
+import java.util.List;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+  private final Long memberId;
+
+  public CustomUserDetails(Long memberId) {
+    this.memberId = memberId;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of();
+  }
+
+  @Override
+  public String getPassword() {
+    return "";
+  }
+
+  @Override
+  public String getUsername() {
+    return "";
+  }
+}
