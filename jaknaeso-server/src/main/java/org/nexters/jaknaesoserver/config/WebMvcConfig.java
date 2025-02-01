42683edc13a2560f5c74162889f34f97d7f5e0dd
NaMinhyeok <nmh9097@gmail.com>
2025-02-01 21:35:37 +0900

    [#40] chore: cors 설정 추가

--- jaknaeso-server/src/main/java/org/nexters/jaknaesoserver/config/WebMvcConfig.java
+++ jaknaeso-server/src/main/java/org/nexters/jaknaesoserver/config/WebMvcConfig.java
@@ -8,6 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+  private static final String LOCAL_URL = "http://localhost:3000";
+  private static final String JAKNAESO_WEB_VERCEL_APP = "https://jaknaeso-web.vercel.app";
+
   @Value("${cors.origins.api-doc}")
   private String API_DOC_HOST;
 
@@ -15,7 +18,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry
         .addMapping("/**")
-        .allowedOrigins(API_DOC_HOST)
+        .allowedOrigins(API_DOC_HOST, LOCAL_URL, JAKNAESO_WEB_VERCEL_APP)
         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
         .allowedHeaders("*");
   }
