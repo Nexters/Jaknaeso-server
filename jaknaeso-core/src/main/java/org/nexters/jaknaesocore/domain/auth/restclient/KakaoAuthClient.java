d7124da142c52f746e5757f6c9e681d2c9162255
kimyu0218 <serena35@naver.com>
2025-02-02 21:21:29 +0900

    [#46] fix: form urlencoded 오류 수정
    - form url encoded는 request body로 보낼 수 없음
    - RequestBody 대신 RequestParam으로 전송

--- jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/auth/restclient/KakaoAuthClient.java
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/auth/restclient/KakaoAuthClient.java
@@ -1,14 +1,21 @@
 package org.nexters.jaknaesocore.domain.auth.restclient;
 
-import org.nexters.jaknaesocore.domain.auth.restclient.dto.KakaoTokenCommand;
 import org.nexters.jaknaesocore.domain.auth.restclient.dto.KakaoTokenResponse;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
 
 @HttpExchange(url = "https://kauth.kakao.com")
 public interface KakaoAuthClient {
 
-  @PostExchange("/oauth/token")
-  KakaoTokenResponse requestToken(@RequestBody KakaoTokenCommand command);
+  @PostExchange(
+      url = "/oauth/token",
+      contentType = MediaType.APPLICATION_FORM_URLENCODED_VALUE + ";charset=utf-8")
+  KakaoTokenResponse requestToken(
+      @RequestParam("grant_type") String grantType,
+      @RequestParam("client_id") String clientId,
+      @RequestParam("client_secret") String clientSecret,
+      @RequestParam("code") String code,
+      @RequestParam("redirect_uri") String redirectUri);
 }
