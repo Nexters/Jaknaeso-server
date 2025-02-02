b7d34f290039eca6cb2ae356c998fc4389997232
kimyu0218 <serena35@naver.com>
2025-02-03 01:33:12 +0900

    [#46] [#35] fix: request param -> MultiValueHashMap
    FormHttpMessageConverter가 request body의 MultiValueHashMap을 x-www-formurlencoded로 변환할 수 있도록 수정

--- jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/auth/restclient/KakaoAuthClient.java
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/auth/restclient/KakaoAuthClient.java
@@ -1,7 +1,9 @@
 package org.nexters.jaknaesocore.domain.auth.restclient;
 
+import org.nexters.jaknaesocore.domain.auth.restclient.dto.KakaoTokenResponse;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
 
@@ -11,10 +13,5 @@ public interface KakaoAuthClient {
   @PostExchange(
       url = "/oauth/token",
       contentType = MediaType.APPLICATION_FORM_URLENCODED_VALUE + ";charset=utf-8")
-  Object requestToken(
-      @RequestParam("grant_type") String grantType,
-      @RequestParam("client_id") String clientId,
-      @RequestParam("client_secret") String clientSecret,
-      @RequestParam("code") String code,
-      @RequestParam("redirect_uri") String redirectUri);
+  KakaoTokenResponse requestToken(@RequestBody MultiValueMap<String, String> params);
 }
