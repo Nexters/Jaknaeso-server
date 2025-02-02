f94ac94876e368dcf01a1b80b638c8148cf12e51
kimyu0218 <serena35@naver.com>
2025-02-03 01:21:08 +0900

    [#35] refactor: fetch join 추가
    연관 관계에 있는 social account도 함께 soft delete 하기 위함

--- jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/member/repository/MemberRepository.java
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/member/repository/MemberRepository.java
@@ -5,9 +5,13 @@ import org.nexters.jaknaesocore.common.support.error.CustomException;
 import org.nexters.jaknaesocore.common.support.error.ErrorType;
 import org.nexters.jaknaesocore.domain.member.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+  @Query(
+      "SELECT m FROM Member m LEFT JOIN FETCH m.socialAccounts "
+          + "WHERE m.id = :id AND m.deletedAt IS NULL")
   Optional<Member> findByIdAndDeletedAtIsNull(final Long id);
 
   default Member findMember(final Long memberId) {
