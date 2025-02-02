234f2ec123a5a4af5c016a08c2e4947eaa7afcf3
kimyu0218 <serena35@naver.com>
2025-02-02 21:29:04 +0900

    [#46] chore: 디버깅을 위한 로그 추가
    ClientHttpRespose를 Object로 변환하여 카카오 API 호출 에러 정보 상세 출력

--- jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/auth/restclient/errorhandler/KakaoClientErrorHandler.java
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/auth/restclient/errorhandler/KakaoClientErrorHandler.java
@@ -1,16 +1,21 @@
 package org.nexters.jaknaesocore.domain.auth.restclient.errorhandler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.nexters.jaknaesocore.common.http.errorhandler.RestClientErrorHandler;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.RestClientException;
 
+@Slf4j
 public class KakaoClientErrorHandler implements RestClientErrorHandler {
 
   @Override
   public void handleError(
       HttpStatusCode statusCode, HttpRequest request, ClientHttpResponse response) {
+    Object errorResponse = (Object) response;
+    log.error(errorResponse.toString());
+
     if (statusCode.value() == 401) {
       throw new RestClientException("인증에 실패했습니다. 카카오 API 토큰을 확인해주세요.");
     }
