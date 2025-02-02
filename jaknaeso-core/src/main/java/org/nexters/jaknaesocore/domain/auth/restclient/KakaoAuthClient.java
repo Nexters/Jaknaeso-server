394481fe1a1b6f574ab6149c673c3b6914c1f98a
kimyu0218 <serena35@naver.com>
2025-02-02 22:52:17 +0900

    [#46] chore: 디버깅을 위해 반환형 Object로 변경

--- jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/auth/restclient/KakaoAuthClient.java
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/auth/restclient/KakaoAuthClient.java
@@ -1,6 +1,5 @@
 package org.nexters.jaknaesocore.domain.auth.restclient;
 
-import org.nexters.jaknaesocore.domain.auth.restclient.dto.KakaoTokenResponse;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.HttpExchange;
@@ -12,7 +11,7 @@ public interface KakaoAuthClient {
   @PostExchange(
       url = "/oauth/token",
       contentType = MediaType.APPLICATION_FORM_URLENCODED_VALUE + ";charset=utf-8")
-  KakaoTokenResponse requestToken(
+  Object requestToken(
       @RequestParam("grant_type") String grantType,
       @RequestParam("client_id") String clientId,
       @RequestParam("client_secret") String clientSecret,
--- jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/auth/restclient/errorhandler/KakaoAuthClientErrorHandler.java
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/auth/restclient/errorhandler/KakaoAuthClientErrorHandler.java
@@ -1,18 +1,21 @@
 package org.nexters.jaknaesocore.domain.auth.restclient.errorhandler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.nexters.jaknaesocore.common.http.errorhandler.RestClientErrorHandler;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.RestClientException;
 
-public class KakaoClientErrorHandler implements RestClientErrorHandler {
+@Slf4j
+public class KakaoAuthClientErrorHandler implements RestClientErrorHandler {
 
   @Override
   public void handleError(
       HttpStatusCode statusCode, HttpRequest request, ClientHttpResponse response) {
+    log.warn("Request URI: {}", request.getURI());
     if (statusCode.value() == 401) {
-      throw new RestClientException("인증에 실패했습니다. 카카오 API 토큰을 확인해주세요.");
+      throw new RestClientException("인증에 실패했습니다. 카카오 인가 코드를 확인해주세요.");
     }
     throw new RestClientException("카카오 API 요청 중 알 수 없는 오류가 발생했습니다.");
   }
