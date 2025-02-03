b2819d6c936a2ef08adcdd6b8cc8822e06ec04bb
kimyu0218 <serena35@naver.com>
2025-02-04 01:53:47 +0900

    [#36] feat: 회원 정보 조회 api 작성

--- jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/member/service/MemberService.java
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/member/service/MemberService.java
@@ -3,6 +3,7 @@ package org.nexters.jaknaesocore.domain.member.service;
 import lombok.RequiredArgsConstructor;
 import org.nexters.jaknaesocore.domain.member.model.Member;
 import org.nexters.jaknaesocore.domain.member.repository.MemberRepository;
+import org.nexters.jaknaesocore.domain.member.service.dto.MemberResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,7 +15,13 @@ public class MemberService {
 
   @Transactional
   public void deleteMember(final Long memberId) {
-    Member member = memberRepository.findMember(memberId);
+    final Member member = memberRepository.findMember(memberId);
     member.softDelete();
+  }
+
+  @Transactional(readOnly = true)
+  public MemberResponse getMember(final Long memberId) {
+    final Member member = memberRepository.findMember(memberId);
+    return MemberResponse.of(member);
   }
 }
--- jaknaeso-server/src/test/java/org/nexters/jaknaesoserver/domain/member/controller/MemberControllerTest.java
+++ jaknaeso-server/src/test/java/org/nexters/jaknaesoserver/domain/member/controller/MemberControllerTest.java
@@ -1,20 +1,23 @@
 package org.nexters.jaknaesoserver.domain.member.controller;
 
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+import static com.epages.restdocs.apispec.Schema.schema;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willDoNothing;
-import static org.mockito.BDDMockito.willThrow;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.epages.restdocs.apispec.SimpleType;
 import org.junit.jupiter.api.Test;
-import org.nexters.jaknaesocore.common.support.error.CustomException;
-import org.nexters.jaknaesocore.common.support.error.ErrorType;
+import org.nexters.jaknaesocore.domain.member.service.dto.MemberResponse;
 import org.nexters.jaknaesoserver.common.support.ControllerTest;
 import org.nexters.jaknaesoserver.common.support.WithMockCustomUser;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 class MemberControllerTest extends ControllerTest {
 
@@ -32,7 +35,7 @@ class MemberControllerTest extends ControllerTest {
                 .with(csrf()))
         .andExpect(status().isNoContent())
         .andDo(
-            MockMvcRestDocumentationWrapper.document(
+            document(
                 "delete-member-success",
                 resource(
                     ResourceSnippetParameters.builder()
@@ -43,47 +46,34 @@ class MemberControllerTest extends ControllerTest {
 
   @WithMockCustomUser
   @Test
-  void 아이디에_해당하는_멤버가_존재하지_않아_404_얘외를_반환한다() throws Exception {
+  void 회원_정보를_조회한다() throws Exception {
 
-    willThrow(new CustomException(ErrorType.MEMBER_NOT_FOUND))
-        .given(memberService)
-        .deleteMember(1L);
+    given(memberService.getMember(1L)).willReturn(new MemberResponse("홍길동", "test@example.com"));
 
     mockMvc
         .perform(
-            delete("/api/v1/members/1")
+            MockMvcRequestBuilders.get("/api/v1/members/1")
                 .accept(APPLICATION_JSON)
                 .contentType(APPLICATION_JSON)
                 .with(csrf()))
-        .andExpect(status().isNotFound())
+        .andExpect(status().isOk())
         .andDo(
-            MockMvcRestDocumentationWrapper.document(
-                "delete-member-fail",
+            document(
+                "get-member-success",
                 resource(
                     ResourceSnippetParameters.builder()
-                        .description("회원 탈퇴")
+                        .description("회원 정보 조회")
                         .tags("Member Domain")
-                        .build())));
-  }
-
-  @WithMockCustomUser(userId = 1)
-  @Test
-  void 본인_계정이_아닌_계정을_삭제하려고_하면_예외를_반환한다() throws Exception {
-
-    mockMvc
-        .perform(
-            delete("/api/v1/members/2")
-                .accept(APPLICATION_JSON)
-                .contentType(APPLICATION_JSON)
-                .with(csrf()))
-        .andExpect(status().is4xxClientError())
-        .andDo(
-            MockMvcRestDocumentationWrapper.document(
-                "delete-member-fail",
-                resource(
-                    ResourceSnippetParameters.builder()
-                        .description("회원 탈퇴")
-                        .tags("Member Domain")
+                        .responseFields(
+                            fieldWithPath("result")
+                                .type(SimpleType.STRING)
+                                .description("API 요청 결과 (성공/실패)"),
+                            fieldWithPath("data.name").type(SimpleType.STRING).description("회원 이름"),
+                            fieldWithPath("data.email")
+                                .type(SimpleType.STRING)
+                                .description("회원 이메일"),
+                            fieldWithPath("error").description("에러").optional())
+                        .responseSchema(schema("MemberResponse"))
                         .build())));
   }
 }
