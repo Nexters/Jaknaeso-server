b837430528cd03e68b683d872246c6ea31399688
NaMinhyeok <nmh9097@gmail.com>
2025-02-03 23:59:26 +0900

    [#31] refactor: final 사용

--- jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/survey/repository/SurveySubmissionRepository.java
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/survey/repository/SurveySubmissionRepository.java
@@ -9,10 +9,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface SurveySubmissionRepository extends JpaRepository<SurveySubmission, Long> {
 
   @EntityGraph(attributePaths = {"survey"})
-  List<SurveySubmission> findByMember_IdAndDeletedAtIsNull(Long memberId);
+  List<SurveySubmission> findByMember_IdAndDeletedAtIsNull(final Long memberId);
 
   Optional<SurveySubmission> findTopByMember_IdAndDeletedAtIsNullOrderByCreatedAtDesc(
-      Long memberId);
+      final Long memberId);
 
-  List<SurveySubmission> findByMember_IdAndSurvey_SurveyBundle_Id(Long memberId, Long bundleId);
+  List<SurveySubmission> findByMember_IdAndSurvey_SurveyBundle_Id(
+      final Long memberId, final Long bundleId);
 }
