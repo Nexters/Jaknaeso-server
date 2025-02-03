4a163411af0081252732eead6e28e85416f336ff
kimyu0218 <serena35@naver.com>
2025-02-03 20:42:45 +0900

    [#46] refactor: 리다이렉트 uri 추가

--- jaknaeso-server/src/main/java/org/nexters/jaknaesoserver/domain/auth/controller/dto/KakaoLoginRequest.java
+++ jaknaeso-server/src/main/java/org/nexters/jaknaesoserver/domain/auth/controller/dto/KakaoLoginRequest.java
@@ -1,11 +1,11 @@
-package org.nexters.jaknaesocore.domain.auth.dto;
+package org.nexters.jaknaesoserver.domain.auth.controller.dto;
 
 import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
 import org.nexters.jaknaesocore.domain.auth.service.dto.KakaoLoginCommand;
 
-public record KakaoLoginRequest(@NotBlank String code) {
+public record KakaoLoginRequest(@NotBlank String code, @NotBlank String redirectUri) {
 
   public KakaoLoginCommand toServiceDto() {
-    return new KakaoLoginCommand(code);
+    return new KakaoLoginCommand(code, redirectUri);
   }
 }
