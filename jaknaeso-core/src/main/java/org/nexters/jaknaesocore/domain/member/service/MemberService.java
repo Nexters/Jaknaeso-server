4cffb173c34a9dd02c69a6def5137377a5e23855
kimyu0218 <serena35@naver.com>
2025-02-03 21:52:22 +0900

    [#35] refactor: 레포지토리 내 비즈니스 로직 삭제
    레포지토리에 있는 비즈니스 로직 -> 서비스로 이동

--- jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/member/service/MemberService.java
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/member/service/MemberService.java
@@ -1,6 +1,7 @@
 package org.nexters.jaknaesocore.domain.member.service;
 
 import lombok.RequiredArgsConstructor;
+import org.nexters.jaknaesocore.domain.member.model.Member;
 import org.nexters.jaknaesocore.domain.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,7 +14,7 @@ public class MemberService {
 
   @Transactional
   public void deleteMember(final Long memberId) {
-    memberRepository.findMember(memberId);
-    memberRepository.softDeleteById(memberId);
+    Member member = memberRepository.findMember(memberId);
+    member.softDelete();
   }
 }
