a3506e1122200c37966acedd8277580625a2c3a7
pythonstrup <pythonstrup@gmail.com>
2025-02-06 22:55:11 +0900

    [#64] fix: cors 오류 수정

---
+++ jaknaeso-server/src/main/java/org/nexters/jaknaesoserver/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package org.nexters.jaknaesoserver.config;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+  private static final String LOCAL_URL = "http://localhost:3000";
+  private static final String JAKNAESO_WEB_VERCEL_APP = "https://jaknaeso-web.vercel.app";
+
+  @Value("${cors.origins.api-doc}")
+  private String API_DOC_HOST;
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+
+    final CorsConfiguration corsConfiguration = new CorsConfiguration();
+    corsConfiguration.setAllowedOrigins(List.of(API_DOC_HOST, LOCAL_URL, JAKNAESO_WEB_VERCEL_APP));
+    corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
+    corsConfiguration.addAllowedHeader("*");
+    corsConfiguration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", corsConfiguration);
+    return source;
+  }
+}
