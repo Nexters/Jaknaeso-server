076d97cdd9daaa504e735877b2d18bd813f230ec
kimyu0218 <serena35@naver.com>
2025-02-08 15:36:21 +0900

    [#71] feat: 멤버의 설문 응답 내역 번들과 함께 조회

--- jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/survey/repository/SurveySubmissionRepository.java
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/survey/repository/SurveySubmissionRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.nexters.jaknaesocore.domain.survey.model.SurveySubmission;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SurveySubmissionRepository extends JpaRepository<SurveySubmission, Long> {
 
@@ -16,4 +17,10 @@ public interface SurveySubmissionRepository extends JpaRepository<SurveySubmissi
 
   List<SurveySubmission> findByMember_IdAndSurvey_SurveyBundle_Id(
       final Long memberId, final Long bundleId);
+
+  @Query(
+      "SELECT s FROM SurveySubmission s "
+          + "JOIN FETCH s.member sm JOIN FETCH s.survey ss JOIN FETCH ss.surveyBundle sb "
+          + "WHERE sm.id = :memberId AND s.deletedAt IS NULL")
+  List<SurveySubmission> findSurveyBundlesByMemberIdAndDeletedAtIsNull(final Long memberId);
 }
