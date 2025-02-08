f2752b84c34417cbad90484def13fbfbe4a404fd
NaMinhyeok <nmh9097@gmail.com>
2025-02-08 14:34:32 +0900

    [#25] refactor: submittedAt 객체화

--- jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/survey/model/SurveyRecord.java
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/survey/model/SurveyRecord.java
@@ -1,20 +1,16 @@
 package org.nexters.jaknaesocore.domain.survey.model;
 
-import java.time.format.DateTimeFormatter;
 import lombok.Builder;
 
 @Builder
 public record SurveyRecord(
     String question, String answer, String retrospective, String submittedAt) {
 
-  private static final DateTimeFormatter DATE_TIME_FORMATTER =
-      DateTimeFormatter.ofPattern("yyyy.MM.dd");
-
   public static SurveyRecord of(SurveySubmission submission) {
     String question = submission.getSurvey().getContent();
     String answer = submission.getSelectedOption().getContent();
     String retrospective = submission.getRetrospective();
-    String submittedAt = submission.getSubmittedAt().format(DATE_TIME_FORMATTER);
+    String submittedAt = submission.getYearMonthDay();
     return new SurveyRecord(question, answer, retrospective, submittedAt);
   }
 }
