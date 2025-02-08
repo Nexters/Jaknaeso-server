aa47692998f9858556da64d3958f74568c951ac3
kimyu0218 <serena35@naver.com>
2025-02-08 15:38:30 +0900

    [#71] feat: 회원 캐릭터 목록을 조회하는 비즈니스 로직 작성

---
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/character/service/CharacterService.java
@@ -0,0 +1,35 @@
+package org.nexters.jaknaesocore.domain.character.service;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import lombok.RequiredArgsConstructor;
+import org.nexters.jaknaesocore.domain.character.service.dto.CharacterResponse;
+import org.nexters.jaknaesocore.domain.member.repository.MemberRepository;
+import org.nexters.jaknaesocore.domain.survey.model.SurveyBundle;
+import org.nexters.jaknaesocore.domain.survey.repository.SurveySubmissionRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CharacterService {
+
+  private final MemberRepository memberRepository;
+  private final SurveySubmissionRepository surveySubmissionRepository;
+
+  public List<CharacterResponse> getCharacters(final Long memberId) {
+    memberRepository.findMember(memberId);
+
+    AtomicLong ordinalNumber = new AtomicLong(1);
+    return getSurveyBundles(memberId).stream()
+        .map(it -> new CharacterResponse(ordinalNumber.getAndIncrement(), it.getId()))
+        .toList();
+  }
+
+  private List<SurveyBundle> getSurveyBundles(final Long memberId) {
+    return surveySubmissionRepository
+        .findSurveyBundlesByMemberIdAndDeletedAtIsNull(memberId)
+        .stream()
+        .map(it -> it.getSurvey().getSurveyBundle())
+        .toList();
+  }
+}
