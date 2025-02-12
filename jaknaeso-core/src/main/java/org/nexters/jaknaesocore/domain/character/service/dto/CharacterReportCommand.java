91f17c4e404f60a7f9faac872581d71a50bd363a
kimyu0218 <serena35@naver.com>
2025-02-12 23:50:13 +0900

    [#57] feat: dto, api 작성

---
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/character/service/dto/CharacterReportCommand.java
@@ -0,0 +1,3 @@
+package org.nexters.jaknaesocore.domain.character.service.dto;
+
+public record CharacterReportCommand(Long memberId, Long bundleId) {}
--- jaknaeso-server/src/main/java/org/nexters/jaknaesoserver/domain/character/controller/CharacterController.java
+++ jaknaeso-server/src/main/java/org/nexters/jaknaesoserver/domain/character/controller/CharacterController.java
@@ -3,6 +3,8 @@ package org.nexters.jaknaesoserver.domain.character.controller;
 import lombok.RequiredArgsConstructor;
 import org.nexters.jaknaesocore.common.support.response.ApiResponse;
 import org.nexters.jaknaesocore.domain.character.service.CharacterService;
+import org.nexters.jaknaesocore.domain.character.service.dto.CharacterReportCommand;
+import org.nexters.jaknaesocore.domain.character.service.dto.CharacterReportResponse;
 import org.nexters.jaknaesocore.domain.character.service.dto.CharactersResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +19,16 @@ public class CharacterController {
   private final CharacterService characterService;
 
   @GetMapping
-  public ApiResponse<CharactersResponse> getCharacters(@RequestParam Long memberId) {
+  public ApiResponse<CharactersResponse> getCharacters(@RequestParam final Long memberId) {
     CharactersResponse response = characterService.getCharacters(memberId);
+    return ApiResponse.success(response);
+  }
+
+  @GetMapping("/report")
+  public ApiResponse<CharacterReportResponse> getCharacterReport(
+      @RequestParam final Long memberId, @RequestParam final Long bundleId) {
+    CharacterReportResponse response =
+        characterService.getCharacterReport(new CharacterReportCommand(memberId, bundleId));
     return ApiResponse.success(response);
   }
 }
