23ab1d88637a9e64256c37ab20698d884317c9f7
kimyu0218 <serena35@naver.com>
2025-02-12 23:09:36 +0900

    [#57] feat: 캐릭터 목록, 결과 조회 비즈니스 로직 작성

---
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/character/repository/CharacterRecordRepository.java
@@ -0,0 +1,23 @@
+package org.nexters.jaknaesocore.domain.character.repository;
+
+import java.util.List;
+import java.util.Optional;
+import org.nexters.jaknaesocore.domain.character.model.CharacterRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface CharacterRecordRepository extends JpaRepository<CharacterRecord, Long> {
+
+  @Query(
+      "SELECT c FROM CharacterRecord c "
+          + "JOIN FETCH c.member cm JOIN FETCH c.surveyBundle cs JOIN FETCH c.valueReports "
+          + "WHERE cm.id = :memberId AND cs.id = :bundleId AND c.deletedAt IS NULL")
+  Optional<CharacterRecord> findWithSurveyBundleByMemberIdAndBundleIdAndDeletedAtIsNull(
+      final Long memberId, final Long bundleId);
+
+  @Query(
+      "SELECT c FROM CharacterRecord c "
+          + "JOIN FETCH c.member cm JOIN FETCH c.surveyBundle "
+          + "WHERE cm.id = :memberId AND c.deletedAt IS NULL")
+  List<CharacterRecord> findWithSurveyBundleByMemberIdAndDeletedAtIsNull(final Long memberId);
+}
--- jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/character/service/CharacterService.java
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/character/service/CharacterService.java
@@ -1,38 +1,51 @@
 package org.nexters.jaknaesocore.domain.character.service;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import lombok.RequiredArgsConstructor;
+import org.nexters.jaknaesocore.common.support.error.CustomException;
+import org.nexters.jaknaesocore.domain.character.model.CharacterRecord;
+import org.nexters.jaknaesocore.domain.character.repository.CharacterRecordRepository;
+import org.nexters.jaknaesocore.domain.character.service.dto.CharacterReportCommand;
+import org.nexters.jaknaesocore.domain.character.service.dto.CharacterReportResponse;
 import org.nexters.jaknaesocore.domain.character.service.dto.CharactersResponse;
 import org.nexters.jaknaesocore.domain.character.service.dto.CharactersResponse.CharacterResponse;
 import org.nexters.jaknaesocore.domain.member.repository.MemberRepository;
-import org.nexters.jaknaesocore.domain.survey.model.SurveyBundle;
-import org.nexters.jaknaesocore.domain.survey.repository.SurveySubmissionRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class CharacterService {
 
   private final MemberRepository memberRepository;
-  private final SurveySubmissionRepository surveySubmissionRepository;
+  private final CharacterRecordRepository characterRecordRepository;
 
+  @Transactional(readOnly = true)
   public CharactersResponse getCharacters(final Long memberId) {
     memberRepository.findMember(memberId);
 
-    AtomicLong ordinalNumber = new AtomicLong(1L);
-    List<CharacterResponse> characters =
-        getSurveyBundles(memberId).stream()
-            .map(it -> new CharacterResponse(ordinalNumber.getAndIncrement(), it.getId()))
+    final List<CharacterResponse> characters =
+        characterRecordRepository
+            .findWithSurveyBundleByMemberIdAndDeletedAtIsNull(memberId)
+            .stream()
+            .map(it -> new CharacterResponse(it.getCharacterNo(), it.getSurveyBundle().getId()))
             .toList();
     return new CharactersResponse(characters);
   }
 
-  private List<SurveyBundle> getSurveyBundles(final Long memberId) {
-    return surveySubmissionRepository
-        .findWithSurveyBundlesByMemberIdAndDeletedAtIsNull(memberId)
-        .stream()
-        .map(it -> it.getSurvey().getSurveyBundle())
-        .toList();
+  @Transactional(readOnly = true)
+  public CharacterReportResponse getCharacterReport(final CharacterReportCommand command) {
+    final CharacterRecord characterRecord =
+        characterRecordRepository
+            .findWithSurveyBundleByMemberIdAndBundleIdAndDeletedAtIsNull(
+                command.memberId(), command.bundleId())
+            .orElseThrow(() -> CustomException.CHARACTER_RECORD_NOT_FOUND);
+    return CharacterReportResponse.builder()
+        .type("TODO")
+        .description("TODO")
+        .startDate(characterRecord.getStartDate())
+        .endDate(characterRecord.getEndDate())
+        .valueReports(characterRecord.getValueReports())
+        .build();
   }
 }
