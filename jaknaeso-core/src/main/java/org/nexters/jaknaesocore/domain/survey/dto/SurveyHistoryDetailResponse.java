54e603eeee6d7c3cecd49f9903ce0bf6541cd2b2
NaMinhyeok <nmh9097@gmail.com>
2025-02-14 21:21:03 +0900

    [#84] feat: 회차 정보 추가

--- jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/survey/dto/SurveyHistoryDetailResponse.java
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/survey/dto/SurveyHistoryDetailResponse.java
@@ -1,8 +1,8 @@
 package org.nexters.jaknaesocore.domain.survey.dto;
 
-public record SurveyHistoryDetailResponse(Long submissionId) {
+public record SurveyHistoryDetailResponse(Long submissionId, int index) {
 
-  public static SurveyHistoryDetailResponse of(Long submissionId) {
-    return new SurveyHistoryDetailResponse(submissionId);
+  public static SurveyHistoryDetailResponse of(Long submissionId, int index) {
+    return new SurveyHistoryDetailResponse(submissionId, index);
   }
 }
