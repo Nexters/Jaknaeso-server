83f4a39e393df717f0c96d646b84ef1febf9efba
kimyu0218 <serena35@naver.com>
2025-02-14 20:30:32 +0900

    [#57] chore: ddl 변경 사항 반영

--- jaknaeso-core/src/main/resources/sql/057/001.create_table_value_report.sql
+++ jaknaeso-core/src/main/resources/sql/057/001.create_table_value_report.sql
@@ -1,6 +1,6 @@
 create table if not exists value_reports
 (
-  percentage    decimal(5,2),
-  character_id  bigint        not null,
-  keyword       varchar(30)
+  percentage                decimal(5,2),
+  keyword                   varchar(30),
+  character_value_report_id  bigint        not null
 );
