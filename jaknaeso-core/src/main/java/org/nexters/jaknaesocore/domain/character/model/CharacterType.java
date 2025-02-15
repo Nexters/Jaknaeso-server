ce0615821070dfdd7b4a537ad6fdc88191a6a763
kimyu0218 <serena35@naver.com>
2025-02-15 23:54:27 +0900

    [#93] refactor: EqualsAndHashCode 어노테이션 삭제

--- jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/character/model/CharacterType.java
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/character/model/CharacterType.java
@@ -3,14 +3,13 @@ package org.nexters.jaknaesocore.domain.character.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import java.util.Objects;
 import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.nexters.jaknaesocore.common.model.BaseEntity;
 import org.nexters.jaknaesocore.domain.survey.model.Keyword;
 
-@EqualsAndHashCode
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
@@ -26,5 +25,17 @@ public class CharacterType extends BaseEntity {
   public CharacterType(final String name, final String description, final Keyword keyword) {
     this.name = name;
     this.description = description;
+  }
+
+  @Override
+  public final boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null) {
+      return false;
+    }
+    CharacterType that = (CharacterType) o;
+    return getId() != null && Objects.equals(getId(), that.getId());
   }
 }
