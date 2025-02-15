aff044b64dd759bc9f43ee6dbf25fe1543fd58b1
kimyu0218 <serena35@naver.com>
2025-02-16 02:10:02 +0900

    [#93] refactor: equals 상위 클래스로 이동

--- jaknaeso-core/src/main/java/org/nexters/jaknaesocore/common/model/BaseEntity.java
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/common/model/BaseEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import java.util.Objects;
 import lombok.Getter;
 
 @Getter
@@ -13,4 +14,16 @@ public abstract class BaseEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Override
+  public final boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null) {
+      return false;
+    }
+    BaseEntity that = (BaseEntity) o;
+    return getId() != null && Objects.equals(getId(), that.getId());
+  }
 }
