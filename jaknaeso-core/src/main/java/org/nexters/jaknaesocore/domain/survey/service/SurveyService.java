85a1eaa90817ddef9b5394e3660924285e54bdbb
kimyu0218 <serena35@naver.com>
2025-02-16 23:39:18 +0900

    [#95] feat: 캐릭터 생성 이벤트 호출

--- jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/survey/service/SurveyService.java
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/domain/survey/service/SurveyService.java
@@ -2,7 +2,10 @@ package org.nexters.jaknaesocore.domain.survey.service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -10,12 +13,29 @@ import org.nexters.jaknaesocore.common.model.BaseEntity;
 import org.nexters.jaknaesocore.common.support.error.CustomException;
 import org.nexters.jaknaesocore.domain.member.model.Member;
 import org.nexters.jaknaesocore.domain.member.repository.MemberRepository;
-import org.nexters.jaknaesocore.domain.survey.dto.*;
-import org.nexters.jaknaesocore.domain.survey.model.*;
+import org.nexters.jaknaesocore.domain.survey.dto.OnboardingSubmissionResult;
+import org.nexters.jaknaesocore.domain.survey.dto.OnboardingSubmissionsCommand;
+import org.nexters.jaknaesocore.domain.survey.dto.OnboardingSurveyResponse;
+import org.nexters.jaknaesocore.domain.survey.dto.SurveyHistoryDetailResponse;
+import org.nexters.jaknaesocore.domain.survey.dto.SurveyHistoryResponse;
+import org.nexters.jaknaesocore.domain.survey.dto.SurveyResponse;
+import org.nexters.jaknaesocore.domain.survey.dto.SurveySubmissionCommand;
+import org.nexters.jaknaesocore.domain.survey.dto.SurveySubmissionHistoryCommand;
+import org.nexters.jaknaesocore.domain.survey.dto.SurveySubmissionHistoryResponse;
+import org.nexters.jaknaesocore.domain.survey.model.KeywordScores;
+import org.nexters.jaknaesocore.domain.survey.model.OnboardingSurvey;
+import org.nexters.jaknaesocore.domain.survey.model.Survey;
+import org.nexters.jaknaesocore.domain.survey.model.SurveyBundle;
+import org.nexters.jaknaesocore.domain.survey.model.SurveyOption;
+import org.nexters.jaknaesocore.domain.survey.model.SurveyRecord;
+import org.nexters.jaknaesocore.domain.survey.model.SurveySubmission;
+import org.nexters.jaknaesocore.domain.survey.model.SurveySubmissions;
 import org.nexters.jaknaesocore.domain.survey.repository.OnboardingSurveyRepository;
 import org.nexters.jaknaesocore.domain.survey.repository.SurveyBundleRepository;
 import org.nexters.jaknaesocore.domain.survey.repository.SurveyRepository;
 import org.nexters.jaknaesocore.domain.survey.repository.SurveySubmissionRepository;
+import org.nexters.jaknaesocore.domain.survey.service.event.CreateCharacterEvent;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,6 +48,7 @@ public class SurveyService {
   private final SurveySubmissionRepository surveySubmissionRepository;
   private final SurveyRepository surveyRepository;
   private final OnboardingSurveyRepository onboardingSurveyRepository;
+  private final ApplicationEventPublisher eventPublisher;
 
   @Transactional(readOnly = true)
   public SurveyResponse getNextSurvey(final Long bundleId, final Long memberId) {
@@ -123,7 +144,7 @@ public class SurveyService {
   public void submitSurvey(SurveySubmissionCommand command, LocalDateTime submittedAt) {
     Survey survey =
         surveyRepository
-            .findById(command.surveyId())
+            .findByIdWithSurveyBundle(command.surveyId())
             .orElseThrow(() -> CustomException.SURVEY_NOT_FOUND);
     SurveyOption surveyOption = survey.getOptionById(command.optionId());
     Member member =
@@ -135,6 +156,15 @@ public class SurveyService {
         SurveySubmission.create(member, survey, surveyOption, command.comment(), submittedAt);
 
     surveySubmissionRepository.save(surveySubmission);
+
+    final SurveyBundle bundle = survey.getSurveyBundle();
+    final List<SurveySubmission> submissions =
+        surveySubmissionRepository
+            .findByMemberIdAndBundleIdAndDeletedAtIsNullWithSurveyAndSurveyBundle(
+                member.getId(), bundle.getId());
+    if (bundle.isAllSubmitted(submissions)) {
+      publishCreateCharacterEvent(member, bundle, submissions);
+    }
   }
 
   @Transactional(readOnly = true)
@@ -174,6 +204,18 @@ public class SurveyService {
 
     surveySubmissionRepository.saveAll(submissions);
     member.completeOnboarding(submittedAt);
+
+    //    FIXME: 확인 후 주석 제거 필요 (온보딩 번들 전달)
+    //    createCharacter(member, null, submissions);
+  }
+
+  private void publishCreateCharacterEvent(
+      final Member member, final SurveyBundle bundle, final List<SurveySubmission> submissions) {
+    final List<Survey> surveys = submissions.stream().map(SurveySubmission::getSurvey).toList();
+    final KeywordScores scores = new KeywordScores(surveys);
+
+    eventPublisher.publishEvent(
+        new CreateCharacterEvent(member, bundle, scores.getKeywordScores(), submissions));
   }
 
   private Member getMember(Long memberId) {
