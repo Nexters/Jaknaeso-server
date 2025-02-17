f2d20b9979c37c3f536bd2625e26f95ea92a084a
kimyu0218 <serena35@naver.com>
2025-02-17 22:56:37 +0900

    [#107] chore: default status handler 삭제

--- jaknaeso-core/src/main/java/org/nexters/jaknaesocore/common/http/httpinterface/HttpInterfaceFactory.java
+++ jaknaeso-core/src/main/java/org/nexters/jaknaesocore/common/http/httpinterface/HttpInterfaceFactory.java
@@ -1,12 +1,7 @@
 package org.nexters.jaknaesocore.common.http.httpinterface;
 
-import java.util.Map;
-import org.nexters.jaknaesocore.common.http.errorhandler.RestClientErrorHandler;
-import org.nexters.jaknaesocore.domain.auth.restclient.errorhandler.KakaoAuthClientErrorHandler;
-import org.nexters.jaknaesocore.domain.auth.restclient.errorhandler.KakaoClientErrorHandler;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.AnnotationUtils;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
@@ -14,7 +9,6 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -23,20 +17,12 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 @Component
 public class HttpInterfaceFactory {
 
-  Map<String, RestClientErrorHandler> errorHandlers =
-      Map.of(
-          "https://kapi.kakao.com",
-          new KakaoClientErrorHandler(),
-          "https://kauth.kakao.com",
-          new KakaoAuthClientErrorHandler());
-
   public <S> S create(final Class<S> httpClient) {
     HttpExchange httpExchange = AnnotationUtils.getAnnotation(httpClient, HttpExchange.class);
 
     validateHttpExchange(httpClient, httpExchange);
 
     String url = httpExchange.url();
-    RestClientErrorHandler errorHandler = getErrorHandler(url);
     RestClient restClient =
         RestClient.builder()
             .baseUrl(url)
@@ -47,10 +33,6 @@ public class HttpInterfaceFactory {
                   converters.add(new FormHttpMessageConverter());
                   converters.add(new MappingJackson2HttpMessageConverter());
                 })
-            .defaultStatusHandler(
-                HttpStatusCode::isError,
-                (request, response) ->
-                    errorHandler.handleError(response.getStatusCode(), request, response))
             .build();
 
     return HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient))
@@ -65,13 +47,5 @@ public class HttpInterfaceFactory {
     if (!StringUtils.hasText(httpExchange.url())) {
       throw new IllegalArgumentException("HttpExchange 애노테이션의 url 값이 설정되지 않았습니다.");
     }
-  }
-
-  private RestClientErrorHandler getErrorHandler(final String url) {
-    return errorHandlers.getOrDefault(
-        url,
-        (statusCode, request, response) ->
-            new RestClientException(
-                String.format("알 수 없는 오류가 발생했습니다. [%s] [%d]", url, statusCode.value())));
   }
 }
