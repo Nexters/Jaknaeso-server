1491ed545f5db71608d7098e16cbfcbd3b4f546b
NaMinhyeok <nmh9097@gmail.com>
2025-02-28 20:13:07 +0900

    [#130] feat: elk 로깅 추가

--- jaknaeso-server/src/main/java/org/nexters/jaknaesoserver/domain/auth/controller/dto/KakaoLoginWithTokenRequest.java
+++ jaknaeso-server/src/main/java/org/nexters/jaknaesoserver/domain/auth/controller/dto/KakaoLoginWithTokenRequest.java
@@ -1,6 +1,6 @@
 package org.nexters.jaknaesoserver.domain.auth.controller.dto;
 
-import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 import org.nexters.jaknaesocore.domain.auth.service.dto.KakaoLoginWithTokenCommand;
 
 public record KakaoLoginWithTokenRequest(@NotBlank String accessToken) {
